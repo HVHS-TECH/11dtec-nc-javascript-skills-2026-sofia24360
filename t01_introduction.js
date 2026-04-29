e050fd727f007bce7bbb38ea72c5a85c0db1066e
sofia24360 <24360ss@hvhs.school.nz>
2026-04-29 02:28:57 +0000

    Apr 29, 2026, 2:28 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -8,7 +8,7 @@ console.log("hello")
 /*****************************
  Main code
 ******************************/
-console.log("Hi"+username+"As of"+"you are"+age+"years old."+"You have"+money+"dollars")
+
 
 
 
--- t02_variables.js
+++ t02_variables.js
@@ -1,19 +1,17 @@
 /****************************
 Name of task: variables
 ****************************/
-console.log("Running t01_introduction.js")
-console.log("hello")
 
 //Variables
-let username="Alice"
-let age= 13;
+let username="Sofia"
+let age= 17;
 let pocketMoney= 20;
 let currentYear= 2026;
 
 /*****************************
  Main code
 ******************************/
-console.log("Hi" + username + "As of" + currentYear + "you are" + age + "years old." + "You have" + pocketMoney + "dollars" )
+console.log("Hi " + username + " As of " + currentYear + " you are " + age + " years old." + " You have " + pocketMoney + " dollars." )
 
 
 
