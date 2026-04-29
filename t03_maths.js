5324fd3e42aedae1b565df607a1942ef223c6ff9
sofia24360 <24360ss@hvhs.school.nz>
2026-04-29 02:49:35 +0000

    Apr 29, 2026, 2:49 AM

--- t03_maths.js
+++ t03_maths.js
@@ -7,11 +7,16 @@ let username="Sofia"
 let age= 17;
 let pocketMoney= 20;
 let currentYear= 2026;
+let oldAge;
+let birthYear;
+oldAge = age + 10;
+birthYear = currentYear - age;
+
 
 /*****************************
  Main code
 ******************************/
-console.log("Hi " + username + " As of " + currentYear + " you are " + age + " years old." + " You have " + pocketMoney + " dollars." )
+console.log("Hi " + username + " As of " + currentYear + " you are " + age + " years old." + "" You have " + pocketMoney + " dollars." )
 
 
 
