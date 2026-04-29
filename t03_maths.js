6fce3757070e9490ae90a05fbf8e49304bc2f6ac
sofia24360 <24360ss@hvhs.school.nz>
2026-04-29 02:56:12 +0000

    Apr 29, 2026, 2:56 AM

--- t03_maths.js
+++ t03_maths.js
@@ -11,13 +11,13 @@ let oldAge;
 let birthYear;
 oldAge = age + 10;
 birthYear = currentYear - age;
-
+money
 
 /*****************************
  Main code
 ******************************/
 console.log("Hi " + username + " As of " + currentYear + " you are " + age + " years old.")
-console.log(" You were born in " + birthYear )
+console.log("You were born in " + birthYear)
 console.log("In 10 years you will be " + oldAge + " years old. "+ "You have " + pocketMoney + " dollars." )
 console.log("You spend half of your money, now you have " + )
 
