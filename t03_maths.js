d9c28aa31e578ec9c2dbdf005f0bf28fae465b3e
sofia24360 <24360ss@hvhs.school.nz>
2026-04-29 02:44:41 +0000

    Apr 29, 2026, 2:44 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,20 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task: Maths
+****************************/
+
+//Variables
+let username="Sofia"
+let age= 17;
+let pocketMoney= 20;
+let currentYear= 2026;
+
+/*****************************
+ Main code
+******************************/
+console.log("Hi " + username + " As of " + currentYear + " you are " + age + " years old." + " You have " + pocketMoney + " dollars." )
+
+
+
+/*******************************
+ Fuctions
+ *******************************/de from task 2 here */
