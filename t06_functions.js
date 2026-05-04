ff6f415f1b309be5b821921f0b6d167dbe74b5bb
sofia24360 <24360ss@hvhs.school.nz>
2026-05-04 23:58:39 +0000

    May 4, 2026, 11:58 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,23 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of task: Functions
+****************************/
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 05");
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Hello</p>";
+OUTPUT.innerHTML += "<p>Second paragraph line</p>";
+OUTPUT.innerHTML += "<p>Third paragraph line</p>";
+
+
+
+
+/*******************************
+ Fuctions
+ *******************************/
