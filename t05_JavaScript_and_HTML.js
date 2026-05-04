81bb5c9ae21479b614e388fb5bf5ccec821b670c
sofia24360 <24360ss@hvhs.school.nz>
2026-05-04 23:46:28 +0000

    May 4, 2026, 11:46 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -2,22 +2,14 @@
 Name of task: JavaScript and HTML
 ****************************/
 
-//Variables
-let username="Sofia"
-let age= 17;
-let pocketMoney= 20;
-let currentYear= 2026;
-let oldAge;
-let birthYear;
-let halfMoney;
-oldAge = age+10;
-birthYear = currentYear-age;
-halfMoney = pocketMoney/2;
+
 
 /*****************************
  Main code
 ******************************/
 console.log("Running task 05")
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 
 
 
