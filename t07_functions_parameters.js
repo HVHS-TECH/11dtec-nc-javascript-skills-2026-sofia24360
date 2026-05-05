49908c28e3b80e2db1c6a099d0bbb469113823ac
sofia24360 <24360ss@hvhs.school.nz>
2026-05-05 00:10:41 +0000

    May 5, 2026, 12:10 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,25 @@
-/* Paste your code from task 6 here */
+/****************************
+Name of task: Functions parameters
+****************************/
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 06");
+
+welcome()
+
+
+
+
+/*******************************
+ Fuctions
+ *******************************/
+
+ function welcome(){
+    // Add a line to the html page
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+ }
