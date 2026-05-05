9859e63698406c2b911b44288a1f19008feb213d
sofia24360 <24360ss@hvhs.school.nz>
2026-05-05 00:00:20 +0000

    May 5, 2026, 12:00 AM

--- t06_functions.js
+++ t06_functions.js
@@ -7,13 +7,9 @@ Name of task: Functions
 /*****************************
  Main code
 ******************************/
-console.log("Running task 05");
+console.log("Running task 06");
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML += "<p>Hello</p>";
-OUTPUT.innerHTML += "<p>Second paragraph line</p>";
-OUTPUT.innerHTML += "<p>Third paragraph line</p>";
+
 
 
 
@@ -21,3 +17,7 @@ OUTPUT.innerHTML += "<p>Third paragraph line</p>";
 /*******************************
  Fuctions
  *******************************/
+
+ function welcome(){
+    
+ }
