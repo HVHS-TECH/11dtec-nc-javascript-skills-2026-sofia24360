06d8630a31445da15627e70cd46514e271dda9ee
sofia24360 <24360ss@hvhs.school.nz>
2026-05-10 22:25:08 +0000

    May 10, 2026, 10:25 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,25 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Input from HTML
+****************************/
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 10");
+
+
+
+
+
+/*******************************
+ Fuctions
+ *******************************/
+
+ function start(){
+    // Add a line to the html page
+    
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+ }
