e15af6dfa20807e04d8b8f4674fcd2e4aefe4e4f
sofia24360 <24360ss@hvhs.school.nz>
2026-05-11 23:40:12 +0000

    May 11, 2026, 11:40 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -13,7 +13,7 @@ console.log("Running task 10");
 <form onsubmit="return false;">
     <label for="forName">Name:</label>
     <input type="text" id="usersname" name="formName">
-   <input type="submit" onClick={getFormInput()}>
+   <input type="submit" onClick= getFormInput()>
 </form>
 
 
