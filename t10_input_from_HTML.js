38a83ea5aecfef004d716657130da5735672fe20
sofia24360 <24360ss@hvhs.school.nz>
2026-05-13 02:51:58 +0000

    May 13, 2026, 2:51 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -10,16 +10,13 @@ Name of task: Input from HTML
 ******************************/
 console.log("Running task 10");
 
-
- 
-
-
+let userName = NAME_FIELD.ariaValueMax;
 /*******************************
  Fuctions
 ********************************/
 function getFormInput(){
     const NAME_FIELD= document.getElementById("nameFeild");
-    let userName = NAME_FIELD.value;
+    
     NAME_FIELD.innerHTML = "<p>Your name is "+ userName +"</p>"
 
 }
