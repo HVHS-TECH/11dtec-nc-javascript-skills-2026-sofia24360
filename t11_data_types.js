c85cbc776d3468d6b3c07e0d11c611ab918db78d
sofia24360 <24360ss@hvhs.school.nz>
2026-05-14 21:18:39 +0000

    May 14, 2026, 9:18 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,24 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Input from HTML
+****************************/
+
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 10");
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+const NAME_FIELD = document.getElementById("nameField")
+
+
+
+/*******************************
+ Fuctions
+********************************/
+function getFormInput(){
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p> Your name is " + userName + "</p>"
+}
