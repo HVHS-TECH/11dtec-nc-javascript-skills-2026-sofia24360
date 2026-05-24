7eb66bf06548265489d59a6700819d4ac0b82c31
sofia24360 <24360ss@hvhs.school.nz>
2026-05-24 22:51:01 +0000

    May 24, 2026, 10:51 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,1 +1,40 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Functions Return
+****************************/
+
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 13");
+
+
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+const POCKET_MONEY = document.getElementById("pocketMoney")
+
+
+/*******************************
+ Fuctions
+********************************/
+function getFormInput(){
+    let userMoney = POCKET_MONEY.value;
+    if (userMoney >= 4){
+        OUTPUT.innerHTML += "<p> A chocolate bar costs $4. You CAN afford a chocolate bar. </p>";
+    calculateChange(userMoney, 4)
+    
+        
+    }
+    else{
+        OUTPUT.innerHTML += "<p> A chocolate bar costs $4. Sorry you CAN'T afford a chocolate bar </p>"
+    }
+}
+
+function calculateChange(_pocketmoney, _price){
+        
+        let change = _pocketmoney - _price;
+        OUTPUT.innerHTML +="<p>You will get $</p>" + change +"<p>change.</p>";
+        return change;
+}
