c804050970dfe10f9ca1873688274c7b98fa32b1
sofia24360 <24360ss@hvhs.school.nz>
2026-05-24 22:38:41 +0000

    May 24, 2026, 10:38 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -14,7 +14,7 @@ console.log("Running task 13");
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 const POCKET_MONEY = document.getElementById("pocketMoney")
-var change = (7,4)
+var change = (,4)
 
 
 /*******************************
@@ -34,5 +34,5 @@ function getFormInput(){
 
 function calculateChange(_pocketmoney, _price){
         let change = _pocketmoney - _price;
-        OUTPUT.innerHTML +="<p>Your change is </p>" + change;
+        OUTPUT.innerHTML +="<p>You will get $</p>" + change;
 }
