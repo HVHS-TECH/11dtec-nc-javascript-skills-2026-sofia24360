a8645ba65c490a59fd85dc9f7904e3db914ce5b4
sofia24360 <24360ss@hvhs.school.nz>
2026-05-24 22:28:42 +0000

    May 24, 2026, 10:28 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -24,6 +24,8 @@ function getFormInput(){
     let userMoney = POCKET_MONEY.value;
     if (userMoney >= 4){
         OUTPUT.innerHTML += "<p> A chocolate bar costs $4. You CAN afford a chocolate bar. </p>";
+    calculatechange()
+        
     }
     else{
         OUTPUT.innerHTML += "<p> A chocolate bar costs $4. Sorry you CAN'T afford a chocolate bar </p>"
@@ -31,8 +33,6 @@ function getFormInput(){
 }
 
 function calculatechange(_pocketmoney, _price){
-    if (userMoney >= 4){
         let change = _pocketmoney - _price;
         OUTPUT.innerHTML +="<p>Your change is </p>" + change
-    }
 }
