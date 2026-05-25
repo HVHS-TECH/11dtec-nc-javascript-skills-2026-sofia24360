3ddb8c8ba2acbebbcf914a22c448d8e64c19f5fa
sofia24360 <24360ss@hvhs.school.nz>
2026-05-25 23:36:29 +0000

    May 25, 2026, 11:36 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -13,13 +13,28 @@ console.log("Running task 14");
 
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
-const USER_RATE = document.getElementById("userRate")
+const POCKET_MONEY = document.getElementById("pocketMoney")
 
 
 /*******************************
  Fuctions
 ********************************/
-getFormInput(){
-    let rateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"]
-    let
+function getFormInput(){
+    let userMoney = POCKET_MONEY.value;
+    if (userMoney >= 4){
+        OUTPUT.innerHTML += "<p> A chocolate bar costs $4. You CAN afford a chocolate bar. </p>";
+    calculateChange(userMoney, 4)
+    
+        
+    }
+    else{
+        OUTPUT.innerHTML += "<p> A chocolate bar costs $4. Sorry you CAN'T afford a chocolate bar </p>"
+    }
+}
+
+function calculateChange(_pocketmoney, _price){
+        
+        let change = _pocketmoney - _price;
+        OUTPUT.innerHTML +="<p>You will get $</p>" + change +"<p>change.</p>";
+        return change;
 }
