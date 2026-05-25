2a37bc057815d6412f5eb984f79d15ca00aaa13e
sofia24360 <24360ss@hvhs.school.nz>
2026-05-25 23:52:34 +0000

    May 25, 2026, 11:52 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -20,7 +20,7 @@ const USER_CHOICE = document.getElementById("userChoice")
  Fuctions
 ********************************/
 function getFormInput(){
-    let rateArray = ["You loath chocolate". "Chocolate is meh", "Chocolate is pretty good","Chocolate is the best thing EVER!!!!"];
+    let rateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good","Chocolate is the best thing EVER!!!!"];
     let choice = USER_CHOICE.value;
     OUTPUT.innerHTML += "You think " + rateArray[choice] + "<br>";
 }
