86c11ca0858898d336ede1e689042351b81dd73e
sofia24360 <24360ss@hvhs.school.nz>
2026-05-26 00:29:02 +0000

    May 26, 2026, 12:29 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -20,6 +20,9 @@ const USER_CHOICE = document.getElementById("userChoice")
  Fuctions
 ********************************/
 function getFormInput(){
+    for(let rateArray = userChoice; rateArray<=3; rateArray= rateArray+1){
+        console.log("Lyrics: " + rateArray)
+    }
     let rateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good","Chocolate is the best thing EVER!!!!"];
     let choice = USER_CHOICE.value;
     OUTPUT.innerHTML += "You think " + rateArray[choice] + "<br>";
