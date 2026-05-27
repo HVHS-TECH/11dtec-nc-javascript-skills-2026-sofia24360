13ecc0e3c6d37932bf66fb0d394b9e2c7971852a
sofia24360 <24360ss@hvhs.school.nz>
2026-05-27 02:31:42 +0000

    May 27, 2026, 2:31 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,26 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Arrays Continued
+****************************/
+
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 16");
+
+
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+const USER_CHOICE = document.getElementById("userChoice")
+
+
+/*******************************
+ Fuctions
+********************************/
+function getFormInput(){
+    let rateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good","Chocolate is the best thing EVER!!!!"];
+    let choice = USER_CHOICE.value;
+    OUTPUT.innerHTML += "You think " + rateArray[choice] + "<br>";
+}
