9d17f6dfa1befb6cd5a79d44b71e18f92e1c27e7
sofia24360 <24360ss@hvhs.school.nz>
2026-06-03 02:43:47 +0000

    Jun 3, 2026, 2:43 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -14,17 +14,19 @@ console.log("Running task 16");
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 const USER_ITEM = document.getElementById("userItem")
-let itemArray = ["milk", "bread", " yogurt", "cookies"];
+
 
 /*******************************
  Fuctions
 ********************************/
-    
+    let itemArray = ["milk", "chocolate", "honey","water"];
 function getFormInput(){
     let choice = USER_ITEM.value;
     itemArray.push("choice")
     OUTPUT.innerHTML += "You have added " + choice + " to the list";
-} 
+}
+function itemList(){
 for(i=0; i<itemArray.length; i++){
-        OUTPUT.innerHTML="These are the items on your shopping list: " + itemArray[i]+"<br>"
+        OUTPUT.innerHTML="These are the items on your shopping list: " + i + itemArray[i]+"<br>"
     }
+}
