12830832395fa0fc66f74f63f26f58dc1279d304
sofia24360 <24360ss@hvhs.school.nz>
2026-06-03 03:06:24 +0000

    Jun 3, 2026, 3:06 AM

--- t18_validation.js
+++ t18_validation.js
@@ -1,1 +1,33 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Arrays Continued
+****************************/
+
+
+
+
+/*****************************
+ Main code
+******************************/
+console.log("Running task 16");
+
+
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+const USER_ITEM = document.getElementById("userItem")
+
+
+/*******************************
+ Fuctions
+********************************/
+    let itemArray = ["milk", "chocolate", "honey","water"];
+function getFormInput(){
+    let choice = USER_ITEM.value;
+    itemArray.push(choice)
+    OUTPUT.innerHTML += "You have added " + choice + " to the list";
+}
+function itemList(){
+    OUTPUT.innerHTML+="<br> These are the items on your shopping list: <br>";
+    for(i=0; i<itemArray.length; i++){
+    OUTPUT.innerHTML+= i + ": " + itemArray[i] + "<br>"
+    }
+}
