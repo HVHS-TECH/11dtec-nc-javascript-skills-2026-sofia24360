ed4bf5df59211ad8c9b56feccb11fda000922e0e
sofia24360 <24360ss@hvhs.school.nz>
2026-06-03 02:37:08 +0000

    Jun 3, 2026, 2:37 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -14,12 +14,12 @@ console.log("Running task 16");
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 const USER_ITEM = document.getElementById("userItem")
-
+let itemArray = ["milk", "bread", " yogurt", "cookies"];
 
 /*******************************
  Fuctions
 ********************************/
-    let itemArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good","Chocolate is the best thing EVER!!!!"];
+    
 function getFormInput(){
     let choice = USER_ITEM.value;
     itemArray.push("choice")
